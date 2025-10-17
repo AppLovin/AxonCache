88fbf4d121324a9b5ee6a532e263c752c152489e
Benjamin Sergeant <benjamin.sergeant@applovin.com>
2025-10-16 17:31:11 -0700

    chdir problem

--- java/mk_jar.sh
+++ java/mk_jar.sh
@@ -8,24 +8,18 @@ mkdir build/java_sandbox
 # Copy whole java folder, required by maven
 cp -rf java build/java_sandbox
 
-cd build/java_sandbox
-
 # Copy the shared libraries. 
 # HACK ALERT
 # Since this script is running on a single platform
 # so that we don't need to detect the current OS, and copy the same shared library in all folders
 for arch in osx-aarch64 linux-x86_64 linux-aarch64
 do
-    mkdir -p java/src/main/resources/natives/$arch
-    cp ../java/libaxoncache_jni.* java/src/main/resources/natives/$arch/
+    mkdir -p build/java_sandbox/java/src/main/resources/natives/$arch
+    cp build/java/libaxoncache_jni.* build/java_sandbox/java/src/main/resources/natives/$arch/
 done
 
 # now run maven
-cd java
-mvn clean package
-cd -
-
-cd -
+(cd java && mvn clean package)
 
 cp build/java_sandbox/java/target/axoncache-java-1.0.0.jar build/java/axoncache.jar
 
