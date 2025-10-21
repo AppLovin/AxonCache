3d3a1500810a8726f6f7ab706346ec1431f654d8
erol-keskiner <erol.keskiner@applovin.com>
2025-10-21 16:22:36 +0200

    Fix clang-format style in Math.h
    - Move return statement to separate line per clang-format rules

--- include/axoncache/Math.h
+++ include/axoncache/Math.h
@@ -12,7 +12,8 @@ namespace math
 
 [[maybe_unused]] [[nodiscard]] static auto roundUpToPowerOfTwo( uint64_t number ) -> uint64_t
 {
-    if ( number <= 1 ) return 1;
+    if ( number <= 1 )
+        return 1;
     return 1UL << ( 64UL - __builtin_clzl( number - 1UL ) );
 }
 
